c2efc481db97bda921902ef563ba056a5b8fdba4
Josh Larson <jplhomer@gmail.com>
2015-08-25 11:42:52 -0500

    Bump version to a newer release

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: 'Dragula - Drag and drop so simple it hurts, packaged for meteor',
   name: 'ahref:dragula',
-  version: '1.0.3',
+  version: '1.1.0',
   git:'https://github.com/rfox90/meteor-dragula'
 });
 
