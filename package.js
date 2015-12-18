c98daa1b0c0374f1e85e83be130b2c415d5c4507
Richard Fox <richard.fox@ahref.co.uk>
2015-12-18 03:23:14 +0000

    Fix submodule to be at 3.5.4, Mirror version number with dragular fixes #6

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: 'Dragula - Drag and drop so simple it hurts, packaged for meteor',
   name: 'ahref:dragula',
-  version: '1.0.6',
+  version: '3.5.4',
   git:'https://github.com/rfox90/meteor-dragula'
 });
 
